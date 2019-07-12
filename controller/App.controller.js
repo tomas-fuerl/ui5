1e2641bbd70a49d703ec735384cdbc10558a2e07
tomas-fuerl <tomasfuerl@googlemail.com>
2019-07-12 11:49:36 +0200

    Schritt 6 und 7 Module und Model

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -1,9 +1,21 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
- ], function (Controller) {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel"
+ ], function (Controller, MessageToast, JSONModel) {
     "use strict";
-    return Controller.extend("hts.itq2017.controller.App"
-    , {      onShowHello : function () {
+    return Controller.extend("hts.itq2017.controller.App", {      
+      onInit : function () {
+         // set data model on view
+         var oData = {
+            recipient : {
+               name : "World"
+            }
+         };
+         var oModel = new JSONModel(oData);
+         this.getView().setModel(oModel);
+      },
+      onShowHello : function () {
           // show a native JavaScript alert
           alert("Hello World");
        }
